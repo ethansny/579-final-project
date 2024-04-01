a3a6a4778593d8389a80bc18295e3c954be415c0
Ethan <ethansnyder@Ethans-MacBook-Pro.local>
2024-04-01 11:28:44 -0400

    fixed config base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://github.com/ethansny/579-final-project"
+  base:"/579-final-project/"
 })
 
