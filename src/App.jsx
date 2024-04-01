f5c598c629f88bb51b55f75f0119b90bac79b930
Ethan <ethansnyder@Ethans-MacBook-Pro.local>
2024-04-01 14:54:23 -0400

    implempented basic weather display

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import SearchField from './components/SearchField/'
+
+
 import './App.css'
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <SearchField />
+      {/* <WeatherData /> */}
     </>
   )
 }
