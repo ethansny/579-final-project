3258bea53f8804d267d31ec81bfc74633c7ac4f7
Ethan Snyder <ethansny@umich.edu>
2024-04-21 11:34:46 -0400

    refinded visuals, made responsive

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -3,17 +3,18 @@ import { useState } from 'react'
 import { Typeahead } from 'react-bootstrap-typeahead';
 import WeatherData from './WeatherData';
 import resorts from "../assets/resorts.js";
-import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
-
+import { ToggleButtonGroup, ToggleButton, Button, Modal } from 'react-bootstrap';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import ErrorModal from './ErrorModal';
+
 
 const filterBy = (option, resort) => {
-    if (resort.selected.length) {
-      return true;
-    }
-    return option.label.toLowerCase().indexOf(resort.text.toLowerCase()) > -1;
+  if (resort.selected.length) {
+    return true;
   }
+  return option.label.toLowerCase().indexOf(resort.text.toLowerCase()) > -1;
+}
 
 
 const SearchField = () => {
@@ -24,6 +25,9 @@ const SearchField = () => {
   const [weatherFilters, setWeatherFilters] = useState(['temperature_2m_max', 'temperature_2m_min', 'snowfall_sum'])
   const endpointRoot = 'https://api.open-meteo.com/v1/forecast?'
   const possibleFilters = ['daylight_duration', 'wind_speed_10m_max', 'wind_gusts_10m_max']
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
 
   function LocationMarker() {
     const [position, setPosition] = useState(null)
@@ -45,13 +49,23 @@ const SearchField = () => {
     )
   }
 
-  const  getWeatherData = async  () => {
-    const filters = [...defaultFilters, ...weatherFilters]
-    const filterString = filters.join(',')
-    const response = await fetch(`${endpointRoot}latitude=${selectedResort[0][0]}&longitude=${selectedResort[0][1]},&daily=,${filterString}&timezone=America%2FNew_York`)
-    const data = await response.json()
-    setWeatherData(data.daily)
-  }
+
+  const getWeatherData = async () => {
+    try {
+      const filters = [...defaultFilters, ...weatherFilters];
+      const filterString = filters.join(',');
+      const response = await fetch(`${endpointRoot}latitude=${selectedResort[0][0]}&longitude=${selectedResort[0][1]},&daily=,${filterString}&timezone=America%2FNew_York`);
+      if (!response.ok) {
+        throw new Error('API request failed');
+      }
+      const data = await response.json();
+      setWeatherData(data.daily);
+      setErrorMessage(''); // Clear the error message if the request was successful
+    } catch (error) {
+      setErrorMessage('Please select a location');
+      setShowModal(true);
+    }
+  };
 
   const updateResort = (e) => {
     if (e.length === 0) {
@@ -62,41 +76,48 @@ const SearchField = () => {
     setSelectedResort([[lat, lon], e[0].label])
   }
 
-    return (
-        <>
-        <MapContainer   maxBounds={[[-90, -180],[90, 180]]} worldCopyJump={true} center={[39.8283, -98.5795]} zoom={2} scrollWheelZoom={true} style={{ width: "70vw", height: "400px", border: "1px solid black" }}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {selectedResort && (
-            <Marker position={[selectedResort[0][0], selectedResort[0][1]]}>
-              <Popup>
-                <b>{selectedResort[1]}</b>
-              </Popup>
-            </Marker>
-          )}
-          <LocationMarker />
-        </MapContainer>
-        <div className="d-flex flex-row bd-highlight mb-3">
-          <Typeahead
-              filterBy={filterBy}
-              id="toggle-example"
-              options={resorts}
-              placeholder="Choose a resort..."
-              onChange={(e) => {
-                updateResort(e)
-                setWeatherData(null)
-              }}
-            />
-            <button onClick={getWeatherData}>Get Weather Data</button>
-        </div>
-        <ToggleButtonGroup type="checkbox" onChange={(newValues) => setWeatherFilters(newValues)}>
-          {possibleFilters.map((filter, index) => <ToggleButton key={filter} id={`tbg-check-${index + 1}`} value={filter}>{filter}</ToggleButton> )}
-        </ToggleButtonGroup>
-        <WeatherData weatherData={weatherData} resort={selectedResort}/>
+  return (
+    <>
+      <MapContainer maxBounds={[[-90, -180], [90, 180]]} worldCopyJump={true} center={[39.8283, -98.5795]} zoom={2} scrollWheelZoom={true} style={{ width: "70vw", height: "400px", border: "1px solid black" }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {selectedResort && (
+          <Marker position={[selectedResort[0][0], selectedResort[0][1]]}>
+            <Popup>
+              <b>{selectedResort[1]}</b>
+            </Popup>
+          </Marker>
+        )}
+        <LocationMarker />
+      </MapContainer>
 
-        </>
-    )
-    }
-  export default SearchField;
+      <div className="d-flex flex-row bd-highlight mb-3">
+        <Typeahead
+          className='searchBar'
+          filterBy={filterBy}
+          id="toggle-example"
+          options={resorts}
+          placeholder="Choose a resort..."
+          selected={selectedResort ? [selectedResort[1]] : []}
+          onChange={(e) => {
+            updateResort(e)
+            setWeatherData(null)
+          }}
+        />
+      </div>
+
+      <div className='buttons'>
+        <button onClick={getWeatherData}>Get Weather Data</button>
+        <ToggleButtonGroup type="checkbox" onChange={(newValues) => setWeatherFilters(newValues)}>
+          {possibleFilters.map((filter, index) => <ToggleButton key={filter} id={`tbg-check-${index + 1}`} value={filter}>{filter}</ToggleButton>)}
+        </ToggleButtonGroup>
+      </div>
+  
+      <WeatherData weatherData={weatherData} resort={selectedResort} />
+      <ErrorModal show={showModal} handleClose={() => setShowModal(false)} errorMessage={errorMessage} />
+    </>
+  )
+}
+export default SearchField;
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -3,54 +3,60 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
 
-const WeatherData = ({weatherData, resort}) => {
+const WeatherData = ({ weatherData, resort }) => {
 
-    // const renderWeatherData = () => {
-    //     return weatherData.time.map((day, index) => {
-    //         const maxTemp = weatherData.temperature_2m_max[index];
-    //         const minTemp = weatherData.temperature_2m_min[index];
-    //         const snowfall = weatherData.snowfall_sum[index];
-    //         return (
-    //             <Col key={day}>
-    //             <Stack>
-    //                 <div>{day}</div>
-    //                 <div>Hi: {maxTemp}°C</div>
-    //                 <div>Low: {minTemp}°C</div>
-    //                 <div>Snow: {snowfall}cm</div>
-    //             </Stack>
-    //             </Col>
+  const keyToLabel = {
+    temperature_2m_max: 'High',
+    temperature_2m_min: 'Low',
+    snowfall_sum: 'Total Snow',
+    daylight_duration: 'Daylight',
+    wind_speed_10m_max: 'Wind Speed',
+    wind_gusts_10m_max: 'Wind Gusts',
+  };
 
-    //         );
-    //     });
-    // }
-    const renderWeatherData = () => {
-  return weatherData.time.map((day, index) => {
-    return (
-      <Col key={day}>
-        <Stack>
-          <div>{day}</div>
-          {Object.keys(weatherData).map((key) => {
-            if (key !== 'time') {
-              return <div key={key}>{`${key}: ${weatherData[key][index]}`}</div>;
-            }
-            return null;
-          })}
-        </Stack>
-      </Col>
-    );
-  });
-};
-    return (
-        <div>
-          <h2>Weather Forecast for {resort ? resort[1] : null}</h2>
-          <Container>
-          <Row>
-            {weatherData ? renderWeatherData() : null}
-            </Row>
-          </Container>
+  const formatValue = (key, value) => {
+    if (key === 'temperature_2m_max' || key === 'temperature_2m_min') {
+      return `${value}°C`;
+    } else if (key === 'daylight_duration') {
+      return `${(value / 3600).toFixed(2)} hrs`;
+    } else if (key === 'wind_speed_10m_max' || key === 'wind_gusts_10m_max') {
+      return `${value} km/hr`;
+    } else if (key === 'snowfall_sum') {
+      return `${value} cm`;
+    }
+    return value;
+  };
 
-        </div>
+  const renderWeatherData = () => {
+    return weatherData.time.map((day, index) => {
+      return (
+        <Col key={day}>
+          <Stack>
+            <div>{day}</div>
+            {Object.keys(weatherData).map((key) => {
+              if (key !== 'time') {
+                const label = keyToLabel[key] || key;
+                const value = formatValue(key, weatherData[key][index]);
+                return <p key={key}>{`${label}: ${value}`}</p>;
+              }
+              return null;
+            })}
+          </Stack>
+        </Col>
       );
+    });
+  };
+  return (
+    <div>
+      <h2>Weather Forecast for {resort ? resort[1] : null}</h2>
+      <Container className='weatherContainer'>
+        <Row>
+          {weatherData ? renderWeatherData() : null}
+        </Row>
+      </Container>
+
+    </div>
+  );
 }
 
 export default WeatherData;
